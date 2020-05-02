29684947231f08705100ff8489ac145b9e4802e8
LNTINFOTECH <LNTINFOTECH@INFJHN2052.sacen.lntinfotech.com>
2020-05-02 20:18:48 +0200

    Hello World Git Added

---
+++ TestProject/src/testpackage/TestGit.java
@@ -0,0 +1,12 @@
+package testpackage;
+
+public class TestGit {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello Test Git World !!!");
+		System.out.println();
+	}
+
+}
